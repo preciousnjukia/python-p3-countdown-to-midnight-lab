2b556ac2badb0be578445b816bfea5d3a070b88b
preciousnjukia <preciousnjukia@gmail.com>
2023-10-16 08:58:16 +0300

    Assignment done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+import time
+def countdown(n):
+    for i in range(n, 0, -1):
+        print(f"{i} SECOND(S)!")
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(n):
+    for i in range(n, 0, -1):
+        print(f"{i} SECOND(S)!")
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
